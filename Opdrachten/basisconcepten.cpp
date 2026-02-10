84f7785279f764bbfe94104d93158deba76cdf51
TomWensing <tom@tomwensing.com>
2026-02-10 10:45:36 +0100

    voor 1.3 laat dit de error zien: "expedted '}' at end of input"

--- Opdrachten/basisconcepten.cpp
+++ Opdrachten/basisconcepten.cpp
@@ -3,5 +3,5 @@ using namespace std;
 // Tom Wensing, 1849194
 int main(){
     // print "hello friends!" on the screen: 
-    cout << "hello, friends!\n"
-    } // waow dit is zo cool!
+    cout << "hello, friends!\n";
+     // waow dit is zo cool!
