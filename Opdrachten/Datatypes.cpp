88aba017e6f37486185c9a70fdf872e7ed22d9b1
TomWensing <tom@tomwensing.com>
2026-02-11 09:29:21 +0100

    Datatypes 1.3A

---
+++ Opdrachten/Datatypes.cpp
@@ -0,0 +1,22 @@
+#include <string>
+using namespace std;
+
+int main(){
+
+    int x = 8.2; // x = 8
+    int y = 18 / 5; // y = 3
+    
+    float e = 3.429; // e = 3.429
+    float f = 18 / 5; // f = 3.6
+
+    char a = 'a'; // a = a
+    char b = 'e'; // b = e
+    char c = b; // c = b
+
+    bool r = true; // r = true
+    bool u = (1 & 0); // u = false
+
+    string p = "Klaar?"; // p = Klaar?
+    string s = string("Nee, nog") + string(" niet"); // s = Nee, nog niet
+
+}
