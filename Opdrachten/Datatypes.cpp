72d5c9c42bd46f3fc82b9807ab5318a498c59c93
TomWensing <tom@tomwensing.com>
2026-02-11 09:43:49 +0100

    datatypes 1.3C

--- Opdrachten/Datatypes.cpp
+++ Opdrachten/Datatypes.cpp
@@ -6,24 +6,24 @@ int main(){
 
     int x = 8.2; // x = 8
     int y = 18 / 5; // y = 3
+    int z = 18 / 5.0; // z = 3
+    int f = '8' - '0'; // f = 8
+    int e = 10 % 3; // e = 1
     
-    float e = 3.429; // e = 3.429
-    float f = 18 / 5; // f = 3.6
+    float t = 18 / 5; // t = 3.6
+    float s = 18 / 5.0; // s = 3.6
 
     char a = 'a'; // a = a
     char b = 'e'; // b = e
-    char c = b; // c = b
+    char d = b; // d = e
+    char c = 'b' + ('Z'- 'z'); // c = ?
 
-    bool r = true; // r = true
-    bool u = (1 & 0); // u = false
+    bool r = true && (false || true); // r = true
+    bool u = 8 >= 10; // u = false
 
-    string p = "Klaar?"; // p = Klaar?
-    string s = string("Nee, nog") + string(" niet"); // s = Nee, nog niet
-
-    cout << "x is: " << x << "\n";
-    cout << "e is: " << e << "\n";
-    cout << "a is: " << a << "\n";
-    cout << "c is: " << c << "\n";
-    cout << "u is: " << u << "\n";
-    cout << "s is: " << s << "\n";
+    string p = "Utereg"; // p = Utereg
+    p.append(" Me Stadsie!"); // p = Utereg Me Stadsie
+    string station = string("Heidel") + "berglaan"; // station = Heidelberglaan
+    string plaats = station.erase(10, station.size()-10); // plaats = Heid
+    
 }
